0fa3fcfd73f8f53e012f7d4457b50158be65a8c3
Alexander Willemsen <awm@foreach.be>
2021-12-15 11:39:43 +0100

    added dep

---
+++ src/test/java/com/alex/test/cloudzipkintest/CloudZipkinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.test.cloudzipkintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudZipkinTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
